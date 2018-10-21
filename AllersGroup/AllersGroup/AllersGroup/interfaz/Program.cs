8bdbcf37615affaf83ce568807c390f5dddb89d8
manuelq12 <manuelq12@users.noreply.github.com>
2018-10-20 19:35:01 -0500

    n

--- AllersGroup/AllersGroup/AllersGroup/interfaz/Program.cs
+++ AllersGroup/AllersGroup/AllersGroup/interfaz/Program.cs
@@ -11,25 +11,27 @@ namespace AllersGroup
         static void Main(string[] args)
         {
             ////listo
-            //Console.WriteLine("Introduzca el support mínimo");
-            //double minSup = Convert.ToDouble(Console.ReadLine());
-            //Console.WriteLine("Introduzca la confianza mínima");
-            //double minConf = Convert.ToDouble(Console.ReadLine());
-            //Console.WriteLine("Introduzca el tamaño de agrupaciones de productos [1;7]");
-            //int tamanho = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine("Introduzca el numero de articulos frecuentes que desea");
-            //int numArticulos = Convert.ToInt32(Console.ReadLine());
-            //Controlador controlador = new Controlador(minConf, minSup);
-            //Console.WriteLine("---------------------------------------------------------------------------------------------------------------------  \n");
+            Console.WriteLine("Introduzca el support mínimo");
+            double minSup = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Introduzca la confianza mínima");
+            double minConf = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Introduzca el tamaño de agrupaciones de productos [1;7]");
+            int tamanho = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Introduzca el numero de articulos frecuentes que desea");
+            int numArticulos = Convert.ToInt32(Console.ReadLine());
+            Controlador controlador = new Controlador(minConf, minSup);
+            Console.WriteLine("---------------------------------------------------------------------------------------------------------------------  \n");
 
-            //controlador.CargarDatos();
-            //controlador.generarAsociaciones(tamanho, numArticulos);
-            //controlador.Apriori(tamanho, numArticulos);
-            //Console.WriteLine(controlador.reporte);
-            //Console.Read();
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new InterfazPrincipal());
+            controlador.CargarDatos();
+            controlador.generarAsociaciones(tamanho, numArticulos);
+            controlador.Apriori(tamanho, numArticulos);
+            controlador.FkMinus1XF1(tamanho, numArticulos);
+            Console.WriteLine(controlador.reporte);
+            Console.WriteLine("Termino");
+            Console.Read();
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new InterfazPrincipal());
 
 
         }
--- AllersGroup/AllersGroup/AllersGroup/modelo/Controlador.cs
+++ AllersGroup/AllersGroup/AllersGroup/modelo/Controlador.cs
@@ -66,7 +66,7 @@ namespace AllersGroup
         }
         public int getNumVentas()
         {
-            return ventas.GroupBy(a => a.CardCode).Count();
+            return ventas.GroupBy(a => a.DocNum).Count();
         }
         public int getNumArticulos()
         {
@@ -168,14 +168,14 @@ namespace AllersGroup
                 throw new Exception("Error al cargar las Ventas");
             }
         }
-        public void ImprimirCombinaciones(List<int> n)
+        public String ImprimirCombinaciones(List<int> n)
         {
             String mensaje = "";
             String temp = "";
             int tamanho = n.Count();
             for (int i = 0; i < tamanho; i++)
             {
-                
+
                 int x = n.ElementAt(i);
                 //if (x == 1) temp = "Bread";
                 //else if (x == 2) temp = "Milk";
@@ -184,14 +184,14 @@ namespace AllersGroup
                 //else if (x == 5) temp = "Eggs";
                 //else if (x == 6) temp = "Cola";
                 temp += x;
-                mensaje += (i + 1) + ". " +temp  + "\n";
+                mensaje += (i + 1) + ". " + temp + "\n";
                 temp = "";
-                if(i == tamanho - 1)
+                if (i == tamanho - 1)
                 {
                     mensaje += "\n";
                 }
             }
-            reporte += mensaje;
+            return mensaje;
 
         }
         //Método supremamente extenso.
@@ -495,14 +495,14 @@ namespace AllersGroup
                     foreach (var e in a)
                     {
                         //ImprimirCombinaciones(e);
-                        
+
                         double resSuport = suportPorTamano.ElementAt(loop).ElementAt(loop1);
                         double resConfianza = confianzaPorTamano.ElementAt(loop).ElementAt(loop1);
                         //Console.WriteLine(resConfianza);
 
-                        if (resSuport >= suport && resConfianza >= confianza)
+                        if (resSuport >= suport)
                         {
-                            ImprimirCombinaciones(e);
+                            reporte+= ImprimirCombinaciones(e);
                             reporte+= "Con un Support: " + (resSuport * 100) + "% \nCon una Confianza: " + (resConfianza * 100) + "% \n \n";
                             if (maxS < resSuport && maxC < resConfianza)
                             {
@@ -518,7 +518,7 @@ namespace AllersGroup
                     reporte+= "El maximo para combinaciones de " + (loop + 1) + " elementos es: \n";
                     if (maxId1 != -1 && maxId2 != -1)
                     {
-                        ImprimirCombinaciones(combinacionesPorTamano.ElementAt(maxId1).ElementAt(maxId2));
+                        reporte += ImprimirCombinaciones(combinacionesPorTamano.ElementAt(maxId1).ElementAt(maxId2));
                         reporte+= "Con un Support: " + (maxS * 100) + "% \nCon una Confianza: " + (maxC * 100) + "% \n \n";
                         reporte+="---------------------------------------------------------------------------------------------------------------------  \n";
                         maxS = -1;
@@ -620,32 +620,6 @@ namespace AllersGroup
             List<int> resultados = new List<int>();
             foreach (var combinaciones in todo)
             {
-                //ImprimirCombinaciones(combinaciones);
-                //int tamanho = combinaciones.Count();
-                //int count = 0;
-                //int count2 = 0;
-                //var x = ventas.GroupBy(n => n.CardCode);
-                //foreach (var m in x)
-                //{
-                //    if (m.Count() >= tamanho)
-                //    {
-                //        count2 = 0;
-                //        foreach (var s in m)
-                //        {
-                //            if (combinaciones.Contains(Convert.ToInt32(s.ItemCode)))
-                //            {
-                //                count2++;
-                //            }
-                //        }
-                //        if (count2 == combinaciones.Count)
-                //        {
-                //            count++;
-                //        }
-
-                //    }
-                //}
-                //Console.WriteLine(count);
-                //Console.WriteLine(RepeticionEnVentasUnitario(combinaciones));
                 resultados.Add(RepeticionEnVentasUnitario(combinaciones));
             }
             return resultados;
@@ -666,38 +640,39 @@ namespace AllersGroup
         }
         public int RepeticionEnVentasUnitario(List<int> todo)
         {
-                int tamanho = todo.Count();
-                int count = 0;
-                int count2 = 0;
-                var x = ventas.GroupBy(n => n.CardCode);
-                foreach (var m in x)
+            int countaux = 0;
+            int count = 0;
+            int tamanho = todo.Count();
+            var x = ventas.GroupBy(n => n.DocNum);
+            
+            foreach (var m in x)
+            {
+                if(m.Count() >= tamanho)
                 {
-                    if (m.Count() >= tamanho)
+                    countaux = 0;
+                    foreach (var k in m)
                     {
-                        count2 = 0;
-                        foreach (var s in m)
+                        if (todo.Contains(Convert.ToInt32(k.ItemCode)))
                         {
-                            if (todo.Contains(Convert.ToInt32(s.ItemCode)))
-                            {
-                                count2++;
-                            }
+                            countaux++;
                         }
-                        if (count2 == combinaciones.Count)
-                        {
-                            count++;
-                        }
-
+                    }
+                    if(countaux == tamanho)
+                    {
+                        count++;
                     }
                 }
-              
+            }
             return count;
+       
+
         }
         //FIN APLICACION ESTRATEGIA DE FUERZA BRUTA *************
 
         // APLICACIÓN ESTRATEGIA A-PRIORI *****************
         public void Apriori(int tamanho, int numArti)
         {
-                reporte+= "Tamaño =  " + tamanho + "\n";
+            reporte+= "Tamaño =  " + tamanho + "\n";
             int supCount = (int) (minSuport * getNumVentas());
             reporte+= "Num Ventas = " + getNumVentas() + "\n";
             reporte+= "SupCount = " + supCount + "\n \n";
@@ -731,14 +706,18 @@ namespace AllersGroup
                 }
             }
             reporte+= "Utilizando el método apriori \n \nLas combinaciones de " + tamanho + " elementos que cumplen con el Support Count definido son: \n";
-            temp.ForEach(n => ImprimirCombinaciones(n));
+            temp.ForEach(n => reporte += ImprimirCombinaciones(n));
 
             reporte+="Generación de reglas: \n";
             List<List<List<int>>> reglas = new List<List<List<int>>>();
-            foreach(var n in temp)
+            foreach (var n in temp)
             {
                 List<List<int>> combinacion = AprioriGenRules(n);
-                if(combinacion.Count() != 0)
+                if(combinacion == null)
+                {
+                    reporte += "No se pueden generar reglas no triviales con grupos de 1 elemento \n";
+                }
+                else if(combinacion.Count() != 0)
                 {
                     reglas.Add(combinacion);
                 }
@@ -747,11 +726,11 @@ namespace AllersGroup
 
             if(reglas.Count() == 0)
             {
-                Console.WriteLine("No existe ninguna regla que cumpla con el criterio de confianza");
+               reporte += "No existe ninguna regla que cumpla con el criterio de confianza\n";
             }
             else
             {
-            reglas.ForEach(n => n.ForEach(s =>  ImprimirAsociaciones(s)));
+                reglas.ForEach(s => ImprimirAsociaciones(s));
             }
 
 
@@ -813,59 +792,209 @@ namespace AllersGroup
             double conf = countGrande / countPequeña;
             if(conf >= minCon)
             {
-                if(conf >= 1)
-                {
-                    Console.WriteLine("Dato atípico \n");
-                    ImprimirCombinaciones(temporal);
-                }
                 return temporal;
             }
-            else
-            {
+            else { 
                 return null;
             }
 
         }
-
-        public void ImprimirAsociaciones(List<int> n)
+        public void ImprimirAsociaciones (List<List<int>> asociaciones)
         {
-            String mensaje = "La combinación a continuación cumple con las siguientes asociaciones BASES: \n";
-            int tamanho = n.Count();
-            for (int i = 0; i < tamanho; i++)
+            String mensaje = "---------------------------------------------------------------------------------- \n";
+            List<int> izquierdo = new List<int>();
+            List<int> temp = new List<int>();
+            List<int> derecho = new List<int>();
+            asociaciones.ElementAt(0).ForEach(n => temp.Add(n));
+            if(asociaciones.Count == temp.Count)
             {
-
-                int x = n.ElementAt(i);
-                //if (x == 1) temp = "Bread";
-                //else if (x == 2) temp = "Milk";
-                //else if (x == 3) temp = "Diapers";
-                //else if (x == 4) temp = "Beer";
-                //else if (x == 5) temp = "Eggs";
-                //else if (x == 6) temp = "Cola";
-              
-                if(i == tamanho - 1)
+                mensaje += "Completa \n " + AsociacionCompleta(temp) + "\n";
+            }
+            else
+            {
+                foreach (var n in asociaciones)
                 {
-                    mensaje += x + " \n \n";
+                    derecho.Add(n.Last());
                 }
-                else if(i == tamanho - 2)
+                foreach (var n in temp)
                 {
-                  mensaje += x + " ==> ";
+                    if (!derecho.Contains(n))
+                    {
+                        izquierdo.Add(n);
+                    }
                 }
-                else
+                foreach (var n in izquierdo)
                 {
-                mensaje += x + " - ";
+                    mensaje += n + " ";
 
+                }
+                mensaje += "=> ";
+
+                foreach(var n in derecho)
+                {
+                    mensaje += n + " ";
                 }
             }
-            reporte+= mensaje;
+            reporte += mensaje + "\n";
 
         }
+        public String AsociacionCompleta(List<int> todas)
+        {
+            int[] indices = todas.ToArray();
+            String mensaje = "";
+            String inicio = "";
+            String retorno = "";
+            for (int i = 0; i < indices.Length; i++)
+            {
+                inicio = indices[i] + " ==> ";
+                for(int j = 0; j < indices.Length; j++)
+                {
+                    if( i != j)
+                    {
+                        mensaje += indices[j] + " ";
+                    }
+                }
+                inicio += mensaje + "\n";
+                retorno += inicio;
+                mensaje = "";
+            }
 
+            return retorno;
+        }
 
 
 
         // FIN APLICACIÓN ESTRATEGIA A-PRIORI *******************
 
+        // APLICACIÓN ESTRATEGIA F(k-1)XF1   *****************
 
+       public void FkMinus1XF1(int tamanho,int numArticulos)
+        {
+            reporte += "---------------------------------------------------------------------------------------------\n";
+            reporte += "Método F(k-1) X F1\n";
+            int[] F1 = masFrecuentesMetodo(numArticulos);
+            int supCount = (int) (minSuport * getNumVentas());
+            F1 = F1Check(F1, supCount);
+            reporte += "supCount: " + supCount + "\n";
+            List<List<int>> temp = new List<List<int>>();
+            List<List<int>> temp2 = new List<List<int>>();
+            List<List<int>> resultado = new List<List<int>>();
+            if (tamanho >= 2)
+            {
+                List<List<int>> combinaciones = CombinacionHasta7(tamanho - 1, F1);
+                foreach(var n in combinaciones)
+                {
+                    int count = RepeticionEnVentasUnitario(n);
+                    if(count >= supCount)
+                    {
+                        temp2.Add(n);
+                    }
+                }
+
+               if(temp2.Count() != 0)
+                {
+                    foreach (var s in temp2)
+                    {
+                        for(int m = 0; m < F1.Length; m++)
+                        {
+                            List<int> amp = new List<int>();
+                            s.ForEach(c => amp.Add(c));
+                            if (!s.Contains(F1[m]))
+                            {
+                                amp.Add(F1[m]);
+                                temp.Add(amp);
+                            }
+                        }
+                    }
+                }
+               if(temp.Count != 0)
+                {
+                    foreach (var n in temp)
+                    {
+                        int count = RepeticionEnVentasUnitario(n);
+                        if (count >= supCount)
+                        {
+                            List<int> orden = n.OrderBy(l => l).ToList();
+                            if(resultado.Count != 0)
+                            {
+                                bool repetido = CombinacionRepetida(resultado, orden);
+                                if (!repetido)
+                                {
+                                    resultado.Add(orden);
+                                }
+                            }
+                            else
+                            {
+                                resultado.Add(orden);
+                            }
+
+
+                        }
+                    }
+                }
+                reporte += "Los frequent itemsets que cumplen con el support necesario son:\n";
+                resultado.ForEach(t => reporte += ImprimirCombinaciones(t));
+
+            }
+            else
+            {
+                reporte += "Los frequent itemsets que cumplen con el support necesario son:\n";
+                for(int i = 0; i < F1.Length; i++)
+                {
+                    List<int> uno = new List<int>();
+                    uno.Add(F1[i]);
+                    int count = RepeticionEnVentasUnitario(uno);
+                    if(count >= supCount)
+                    {
+                        resultado.Add(uno);
+                    }
+
+                }
+
+                resultado.ForEach(t => reporte += ImprimirCombinaciones(t));
+            }
+        }
+        public bool CombinacionRepetida(List<List<int>> resultado, List<int> orden)
+        {
+            bool repetido = false;
+            int count = 0;
+            int tamanho = orden.Count();
+            foreach(var m in resultado)
+            {
+                foreach(var n in m)
+                {
+                    if (orden.Contains(n)){
+                        count++;
+                    }
+                }
+                if(count == tamanho)
+                {
+                    repetido = true;
+                    break;
+                }
+                else
+                {
+                    count = 0;
+                }
+                
+            }
+            return repetido;
+
+        }
+
+        public int[] F1Check(int[] masFrecuentes, int supCount)
+        {
+            List<int> resultado = new List<int>();
+            for (int i = 0; i < masFrecuentes.Length; i++)
+            {
+                List < int > uno = new List<int>();
+                uno.Add(masFrecuentes[i]);
+                int count = RepeticionEnVentasUnitario(uno);
+                if (count >= supCount) resultado.Add(masFrecuentes[i]);
+            }
+            return resultado.ToArray();
+        }
+        
 
         //---------------------------------------------------------------------------------------------------------------------------------------------
         //Método para UnitTest
