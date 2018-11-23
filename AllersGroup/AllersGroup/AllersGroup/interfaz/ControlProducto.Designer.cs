67cc6493a410dce8b85bb3b55830c4552cc56dba
david1999erazo <33490042+david1999erazo@users.noreply.github.com>
2018-11-23 15:05:38 -0500

    borrar boton seleccion

--- AllersGroup/AllersGroup/AllersGroup/interfaz/ControlProducto.Designer.cs
+++ AllersGroup/AllersGroup/AllersGroup/interfaz/ControlProducto.Designer.cs
@@ -31,7 +31,6 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.butInforProducto = new System.Windows.Forms.Button();
             this.labelCodigo = new System.Windows.Forms.Label();
-            this.butSelect = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,10 +39,10 @@
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.BackgroundImage = global::AllersGroup.Properties.Resources.medicamento;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(9, 46);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 57);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(168, 152);
+            this.pictureBox1.Size = new System.Drawing.Size(224, 187);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -57,10 +56,10 @@
             this.butInforProducto.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.butInforProducto.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.butInforProducto.ForeColor = System.Drawing.Color.White;
-            this.butInforProducto.Location = new System.Drawing.Point(9, 202);
-            this.butInforProducto.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.butInforProducto.Location = new System.Drawing.Point(68, 248);
+            this.butInforProducto.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.butInforProducto.Name = "butInforProducto";
-            this.butInforProducto.Size = new System.Drawing.Size(81, 32);
+            this.butInforProducto.Size = new System.Drawing.Size(122, 39);
             this.butInforProducto.TabIndex = 1;
             this.butInforProducto.Text = "Información";
             this.butInforProducto.UseVisualStyleBackColor = false;
@@ -70,44 +69,23 @@
             // 
             this.labelCodigo.AutoSize = true;
             this.labelCodigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelCodigo.Location = new System.Drawing.Point(55, 14);
-            this.labelCodigo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelCodigo.Location = new System.Drawing.Point(73, 17);
             this.labelCodigo.Name = "labelCodigo";
-            this.labelCodigo.Size = new System.Drawing.Size(98, 29);
+            this.labelCodigo.Size = new System.Drawing.Size(117, 36);
             this.labelCodigo.TabIndex = 5;
             this.labelCodigo.Text = "Codigo";
             // 
-            // butSelect
-            // 
-            this.butSelect.BackColor = System.Drawing.Color.Transparent;
-            this.butSelect.BackgroundImage = global::AllersGroup.Properties.Resources.BotonOscuro;
-            this.butSelect.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.butSelect.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.butSelect.FlatAppearance.BorderSize = 0;
-            this.butSelect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.butSelect.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.butSelect.ForeColor = System.Drawing.Color.White;
-            this.butSelect.Location = new System.Drawing.Point(96, 202);
-            this.butSelect.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.butSelect.Name = "butSelect";
-            this.butSelect.Size = new System.Drawing.Size(81, 32);
-            this.butSelect.TabIndex = 6;
-            this.butSelect.Text = "Seleccionar";
-            this.butSelect.UseVisualStyleBackColor = false;
-            this.butSelect.Click += new System.EventHandler(this.butSelect_Click);
-            // 
             // ControlProducto
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Transparent;
-            this.Controls.Add(this.butSelect);
             this.Controls.Add(this.labelCodigo);
             this.Controls.Add(this.butInforProducto);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "ControlProducto";
-            this.Size = new System.Drawing.Size(191, 245);
+            this.Size = new System.Drawing.Size(255, 302);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -119,6 +97,5 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button butInforProducto;
         private System.Windows.Forms.Label labelCodigo;
-        private System.Windows.Forms.Button butSelect;
     }
 }
